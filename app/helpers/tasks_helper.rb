40c88624f4a62d50610c2ca34d44828106864981
Ewertton Bravo <ewerttonbravo@gmail.com>
2016-01-09 16:06:26 -0300

    Adicionando status e tipos para tasks, labels e arrumando layout para model team tambem

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,18 @@
 module TasksHelper
+	def label_status(status)
+    label = status
+    html_class = nil
+    case status
+      when Task::TO_DO then
+        html_class = "label label-primary"
+      when Task::DOING then
+        html_class = "label label-warning"
+      when Task::PREVENTED then
+        html_class = "label label-danger"
+      else
+        html_class = "label label-success"
+    end
+    
+    content_tag(:span, label, class: html_class)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ActiveRecord::Base
+
+	TASK_TYPES=[EVOLUTIVA="evolutiva", CORRETIVA="corretiva"]
+	STATUS=[TO_DO="não iniciada", DOING="em andamento", DONE="pronto", PREVENTED="impedido"]
+
+	validates :task_type, :status, :mantis, :description, presence: true
 end
